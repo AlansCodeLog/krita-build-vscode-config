ab6138cd71cf7041a1b7f22a20f242e4c94314c6
Alan North <alanscodelog@gmail.com>
2021-06-28 23:36:12 -0300

    fix: change deps generator to MinGW Makefiles

--- build-deps.bat
+++ build-deps.bat
@@ -3,7 +3,7 @@ call %KRITA_SCRIPTS_DIR%\env.bat
 
 cd /D %DEPS_BUILD_DIR%
 
-ninja
+cmake --build .
 if %errorlevel% neq 0 (
 	if "%BEEP_ON_BUILD_ERROR%"=="TRUE" rundll32 user32.dll,MessageBeep
 	exit /b %errorlevel%
--- configure-deps-prebuilt.bat
+++ configure-deps-prebuilt.bat
@@ -5,9 +5,11 @@ call %KRITA_SCRIPTS_DIR%\env.bat
 mkdir "%DEPS_BUILD_DIR%"
 cd /D "%DEPS_BUILD_DIR%"
 
+@REM Using -G "MinGW Makefiles" because using ninja has no advantage. In fact it has the disadvantage that you must do ninja clean && ninja to force re-install if, for example, you had to delete the install folder for some reason.
+
 cmake ^
 %SOURCE_DIR%\build-tools\ci-deps ^
--G Ninja ^
+-G "MinGW Makefiles" ^
 -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
